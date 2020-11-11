2711741e84a69be3904a793d9ba49fe7bc0349bd
Dh3kon <kb-max@hotmail.com>
2020-11-10 22:34:09 -0300

    Initial Commit

---
+++ src/test/java/com/javatechie/springsecurityjwt/SpringSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechie.springsecurityjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
